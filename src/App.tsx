8da98d7467cb8a0bec21d313a05f7c0a3f80aba6
bgaba <>
2024-09-26 23:00:07 +0200

    setup app inital title and sub title

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { useState } from "react"
 import './App.css'
 
 function App() {
-  
+  const [title, setTitle] = useState("My Task Board")
   return (
-     <h1>My Task Board</h1>
+    <div className='container-fluid' >
+      <div className="col-8 offset-4 mt-5">
+        <h1 className="app-title"><i className="bi bi-journal-check me-2 text-info"></i>{ title }</h1>
+        <p className="app-sub-title ms-5">Tasks to keep organised</p>
+      </div>
+    </div> 
   )
 }
 
