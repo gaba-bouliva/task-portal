913bbe6d5a7b1cc0f379205f85c60eb08471fd38
bgaba <>
2024-09-27 13:21:53 +0200

    initial homepage setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import { useState } from "react"
+import Task from "./components/Task"
 import './App.css'
+
 
 function App() {
   const [title, setTitle] = useState("My Task Board")
   return (
     <div className='container-fluid' >
       <div className="col-8 offset-4 mt-5">
-        <h1 className="app-title"> <img src="/public/Logo.svg" alt="pile of files icon" /> { title }</h1>
+        <h1 className="app-title"> 
+          <img src="/public/Logo.svg" alt="pile of files icon" /> 
+            <span className="mx-2">{ title }</span>
+          <sup><img src="/Edit_duotone.svg" alt="pencil icon" /></sup>
+        </h1>
         <p className="app-sub-title ms-5">Tasks to keep organised</p>
+        <Task name="Task in Progress" status="inprogress"/>
+        <Task name="Task Completed" status="done"/>
+        <Task name="Task Won't Do" status="won't do"/>
+        <Task name="Task To Do" description="Work on a Challenge on devChallenges.io, learn TypeScript." status="todo"/>
+        <div className="x-l-warning p-3 w-50 my-3 rounded d-flex">
+          <span className={'p-2 rounded d-warning'}>
+              <img src='/Add_round_duotone.svg' className='d-flex flex-row-reverse' alt="" />
+            </span>
+          <span className="align-self-center mx-3 fw-semibold">
+            Add new task
+          </span>
+        </div>
       </div>
     </div> 
   )
---
+++ src/components/Task.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+
+interface Props {
+  name: string
+  description?: string
+  status: "todo" | "inprogress" | "done" | "won't do"
+}
+
+type TaskConfig = {
+  bgColor: string
+  statusIcon: string
+  taskIcon: string
+}
+
+
+const task = ({name, description, status}: Props) => {
+  let taskDescription = description ? description : "" 
+
+  let configTask= (): TaskConfig => {
+    switch (status) {
+      case 'todo':
+        return {
+              'bgColor': 'secondary',
+              'statusIcon': 'Add_round_duotone.svg',
+              'taskIcon': 'Time_atack_duotone.svg'
+            } 
+        break;
+      case 'inprogress':
+        return {
+          'bgColor': 'warning',
+          'statusIcon': 'Time_atack_duotone.svg',
+          'taskIcon': 'Time_atack_duotone.svg'
+        }
+        
+        break;
+        case 'done':
+          return {
+            'bgColor': 'success',
+            'statusIcon': 'Done_round_duotone.svg',
+            'taskIcon': 'Time_atack_duotone.svg'
+          }
+          break;
+          default:
+            return {
+              'bgColor': 'danger',
+              'statusIcon': 'close_ring_duotone.svg',
+              'taskIcon': 'close_ring_duotone-1.svg'
+            }
+            break;
+          }
+        }
+    const [taskConfig, setTaskConfig] = useState<TaskConfig>(configTask())
+  return (
+      <>
+         <div className={'l-'+taskConfig.bgColor+' p-3 w-50 my-3 rounded d-flex justify-content-between'}>
+          <span className='align-self-center ms-5'> 
+            <p className='fw-bold'>{name}</p>
+            <p className='col-9'>{taskDescription}</p>
+          </span>
+          { taskDescription.length <= 0 ? 
+            <span className={'p-2 rounded d-'+taskConfig.bgColor}>
+              <img src={'/'+taskConfig.statusIcon} className='d-flex flex-row-reverse' alt="" />
+            </span> : ""
+          }
+
+        </div> 
+      </>
+      
+      )
+}
+
+export default task
